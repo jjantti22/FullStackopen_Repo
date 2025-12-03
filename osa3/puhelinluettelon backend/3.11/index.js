4aeec08aee7541468f64bd2e3209356173a8db32
jjantti22 <jjantti22@student.oulu.fi>
2025-12-03 05:59:07 +0200

    osa3t

--- osa3/puhelinluettelon backend/3.11/index.js
+++ osa3/puhelinluettelon backend/3.11/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+
 const app = express()
 
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
-app.use(cors())
+
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
