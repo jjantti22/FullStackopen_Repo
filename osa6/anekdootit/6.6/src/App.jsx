214998a94e9319e10384d811d015181ba195f9c6
jjantti22 <jjantti22@student.oulu.fi>
2026-05-16 07:32:30 +0300

    osa6-2-1

---
+++ osa6/anekdootit/6.6/src/App.jsx
@@ -0,0 +1,16 @@
+import Filter from './components/Filter'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
